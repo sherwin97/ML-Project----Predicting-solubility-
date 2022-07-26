fb70410b94e743da1bfde4e0009d74d61ed90264
sherwin97 <66246089+sherwin97@users.noreply.github.com>
2022-07-26 22:00:03 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+# load data set, data obtained from delaney paper on ESOL
+data = pd.read_csv(
+    "/Users/sherwinng/Desktop/VS_code/chem/ci034243xsi20040112_053635.txt"
+)
+
+# to obtain LogP, MW, RB TPSA for each mol
+
+
+def desc(smiles):
+    """
+    Take in a list of rdkit object
+    Allow user to obtain LogP, molecular weight and no of rotational
+    bonds
+    """
+    mol_list = [Chem.MolFromSmiles(mol) for mol in data.SMILES]
+
+    base_data = np.arange(1, 1)
+    i = 0
+    for mol in mol_list:
+        desc_MolLogP = Descriptors.MolLogP(mol)
+        desc_Molwt = Descriptors.MolWt(mol)
+        desc_NumRotableBonds = Descriptors.NumRotatableBonds(mol)
+        desc_TPSA = Descriptors.TPSA(mol)
+
+        row = np.array([desc_MolLogP, desc_Molwt, desc_NumRotableBonds, desc_TPSA])
+
+        if i == 0:
+            base_data = row
+
+        else:
+            base_data = np.vstack([base_data, row])
+
+        i += 1
+
+    column_names = ["MolLogP", "MolWt", "NumRotableBonds", "TPSA"]
+    descriptors = pd.DataFrame(data=base_data, columns=column_names)
+
+    return descriptors
+
+
+def AromaticAtoms(m):
+    """
+    Take in an rdkit object, obtain the number of aromatic atoms in a molecule
+    """
+    aromatic_atoms2 = [
+        m.GetAtomWithIdx(i).GetIsAromatic() for i in range(m.GetNumAtoms())
+    ]
+    aa_count = []
+    for i in aromatic_atoms2:
+        if i == True:
+            aa_count.append(i)
+
+    sum_aa_count = sum(aa_count)
+    return sum_aa_count
+
+
+mol_list = [Chem.MolFromSmiles(mol) for mol in data.SMILES]
+df = desc(data.SMILES)  # obtain LogP, Molwt and no. of rot bonds
+desc_AromaticProportion = [
+    AromaticAtoms(mol) / Descriptors.HeavyAtomCount(mol) for mol in mol_list
+]  # obtain aromatic proportion
+df_desc_AromaticProportion = pd.DataFrame(
+    desc_AromaticProportion, columns=["Aromatic Proportion"]
+)
+
+
+X = pd.concat([df, df_desc_AromaticProportion], axis=1)
+y = data.iloc[:, 1]
+
+# to check on the correlation between each factor and solubility. For this project, as a rule of thumb, more than .5 = correlated.
+overall_data = pd.concat([df, df_desc_AromaticProportion, data.iloc[:, 1]], axis=1)
---
+++ linear_model.py
@@ -0,0 +1,11 @@
+from data import X, y
+
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 123)
+model = linear_model.LinearRegression()
+model.fit(X_train, y_train)
+
+
